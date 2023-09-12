8f709cbc65a65a19ed4889f849883afbf1aeac7b
sun <cj-s@163.com>
2023-09-12 15:32:45 +0800

    1

---
+++ samples/apps/copilot-chat-app/webapi/CopilotChat/Models/IntentCategory.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Text.Json.Serialization;
+
+namespace SemanticKernel.Service.CopilotChat.Models;
+
+/// <summary>
+/// 对用户意愿进行分类
+/// </summary>
+public class IntentCategory
+{
+    /// <summary>
+    /// 分类
+    /// </summary>
+    [JsonPropertyName("Category")]
+    public string Category { get; set; }
+
+    [JsonPropertyName("Intent")]
+    public string Intent { get; set; }
+
+
+    public IntentCategory(string category, string intent)
+    {
+        this.Category = category;
+        this.Intent = intent;
+    }
+}
